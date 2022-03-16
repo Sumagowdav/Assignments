3f76715eed83665370101b8332eb0ff0582d5496
Sumagowdav <101113115+Sumagowdav@users.noreply.github.com>
2022-03-16 09:19:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("hii");
+var arr =[11,22,33,44,55]
+var count=0
+for(i=0;i<=4;i++)
+{
+   count++ 
+}
+console.log(count);
+
+let foods =["dosa","idli","fries","chips","mudde","sambar","egg"]
+ console.log(foods[5])
+ console.log(foods.length)
